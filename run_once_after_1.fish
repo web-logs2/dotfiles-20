b54a3d220652468804a95c0e32b8ddbfdedfb4d6
yangrui.0 <yangrui.0@bytedance.com>
2023-07-05 20:14:27 +0800

    up

---
+++ run_once_after_1.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+yes|fish_config theme save 'ayu Mirage'
+yes|fish_config prompt save informative
+
+exit 0
--- run_once_after_1_fish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yes|fish_config theme save 'ayu Mirage'
-yes|fish_config prompt save informative
