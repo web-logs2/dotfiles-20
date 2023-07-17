ecd7a6527f6005c41531d98a403ed8acce151f2b
yangrui.0 <yangrui.0@bytedance.com>
2023-07-17 21:11:18 +0800

    up

--- run_once_after_2.sh
+++ run_once_after_2.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 ssh_config_inject() {
-	if ! grep 'chezmoi injection' ~/.ssh/config; then
-		cat <<EOF >>~/.ssh/config
+	[ ! -d ~/.ssh ] && mkdir ~/.ssh
+	cd ~/.ssh || exit 1
+	[ ! -f config ] && touch config
+	if ! grep 'chezmoi injection' config >/dev/null; then
+		cat <<EOF >config.new
 # -- chezmoi injection begin --
 Include "$HOME/.ssh/myconfig"
 # -- chezmoi injection end --
+
 EOF
+		cat config >>config.new
+		mv -f config.new config
+		echo 'ssh config inject done'
 	fi
-	echo a
 }
 
 ssh_config_inject
